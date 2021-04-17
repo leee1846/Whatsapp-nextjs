363baf3c1045e1bc4c75429018f76da122ba559b
leee1846 <leee1846@gmail.com>
2021-04-17 13:54:37 +0900

    feature: sidebar 기본 태그및 스타일구성

--- components/Sidebar/Sidebar.jsx
+++ components/Sidebar/Sidebar.jsx
@@ -1,9 +1,39 @@
 import * as Styled from "./Sidebar.style";
+//icons
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChatIcon from "@material-ui/icons/Chat";
+import { IconButton } from "@material-ui/core";
+import SearchIcon from "@material-ui/icons/Search";
 
 const Sidebar = () => {
+  const createChat = () => {
+    const input = prompt("함께할 유저의 이메일을 작성하세요.");
+
+    if (!input) return;
+  };
+
   return (
     <Styled.Container>
-      <Styled.Header>사이드바</Styled.Header>
+      <Styled.Header>
+        <Styled.UserAvatar />
+        <Styled.IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </Styled.IconsContainer>
+      </Styled.Header>
+
+      <Styled.Search>
+        <SearchIcon />
+        <Styled.SearchInput placeholder='Search in chats' />
+      </Styled.Search>
+
+      <Styled.SidebarButton onClick={createChat}>
+        Start a new chat
+      </Styled.SidebarButton>
     </Styled.Container>
   );
 };
--- components/Sidebar/Sidebar.style.js
+++ components/Sidebar/Sidebar.style.js
@@ -1,8 +1,51 @@
 import Styled from "styled-components";
+import { Avatar, Button } from "@material-ui/core";
 
 export const Container = Styled.div`
 `;
 
 export const Header = Styled.div`
-  
+  display:flex;
+  position:sticky;
+  top:0;
+  background-color:white;
+  z-index:1;
+  justify-content:space-between;
+  align-items:center;
+  padding:15px;
+  height:80px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+export const UserAvatar = Styled(Avatar)`
+  cursor:pointer;
+
+  &:hover {
+    opacity:0.8;
+  }
+`;
+
+export const IconsContainer = Styled.div`
+`;
+
+export const Search = Styled.div`
+  display:flex;
+  align-items:center;
+  padding:5px;
+  border-radius:2px;
+`;
+
+export const SearchInput = Styled.input`
+  outline-width:0;
+  border: none;
+  flex:1;
+`;
+
+export const SidebarButton = Styled(Button)`
+  width:100%;
+
+  &&& {
+    border-top: 1px solid whitesmoke;
+    border-bottom: 1px solid whitesmoke;
+  }
 `;
