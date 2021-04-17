83e09c141558852b1fa278de70007a87176e268f
leee1846 <leee1846@gmail.com>
2021-04-17 13:12:55 +0900

    Init: 초기 세팅

---
+++ components/Sidebar/Sidebar.jsx
@@ -0,0 +1,11 @@
+import * as Styled from "./Sidebar.style";
+
+const Sidebar = () => {
+  return (
+    <Styled.Container>
+      <Styled.Header>사이드바</Styled.Header>
+    </Styled.Container>
+  );
+};
+
+export default Sidebar;
---
+++ components/Sidebar/Sidebar.style.js
@@ -0,0 +1,8 @@
+import Styled from "styled-components";
+
+export const Container = Styled.div`
+`;
+
+export const Header = Styled.div`
+  
+`;
