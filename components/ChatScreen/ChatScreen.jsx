f4ad15020767584e2511a5a344822ebc3f262b62
leee1846 <leee1846@gmail.com>
2021-04-21 15:03:04 +0900

    style: chatScreen 대략적인 아웃풋

--- components/ChatScreen/ChatScreen.jsx
+++ components/ChatScreen/ChatScreen.jsx
@@ -1,9 +1,37 @@
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "../../firebase";
 import * as Styled from "./ChatScreen.style";
+import { useRouter } from "next/router";
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import AttachFileIcon from "@material-ui/icons/AttachFile";
 
-const ChatScreen = () => {
+const ChatScreen = ({ chat, messages }) => {
+  const [user] = useAuthState(auth);
+  const router = useRouter();
+
   return (
     <Styled.Container>
-      <p>스크린</p>
+      <Styled.Header>
+        <Avatar />
+
+        <Styled.HeaderInformation>
+          <h3>Rec Email</h3>
+          <p>Last seen ...</p>
+        </Styled.HeaderInformation>
+        <Styled.HeaderIcons>
+          <IconButton>
+            <AttachFileIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </Styled.HeaderIcons>
+      </Styled.Header>
+
+      <Styled.MessageContainer>
+        <Styled.EndOfMessage />
+      </Styled.MessageContainer>
     </Styled.Container>
   );
 };
--- components/ChatScreen/ChatScreen.style.js
+++ components/ChatScreen/ChatScreen.style.js
@@ -3,3 +3,34 @@ import Styled from "styled-components";
 export const Container = Styled.div`
   
 `;
+
+export const Header = Styled.div`
+  position:sticky;
+  background-color:white;
+  z-index:100;
+  top:0;
+  display:flex;
+  padding:11px;
+  height:80px;
+  align-items:center;
+  border-bottom:1px solid whitesmoke;
+`;
+
+export const HeaderInformation = Styled.div`
+  margin-left:15px;
+  flex:1;
+
+  & h3 {
+    margin-bottom:3px;
+  }
+  & p {
+    font-size:14px;
+    color:gray;
+  }
+`;
+
+export const HeaderIcons = Styled.div``;
+
+export const MessageContainer = Styled.div``;
+
+export const EndOfMessage = Styled.div``;
